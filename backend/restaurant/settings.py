501ce106d81ee07a8f3914df2badeff964365c7e
Gustavo Linares Aquino <glinares@unsa.edu.pe>
2026-07-21 11:03:44 -0500

    Configure global templates directory

--- backend/restaurant/settings.py
+++ backend/restaurant/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
